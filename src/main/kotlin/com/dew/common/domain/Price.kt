b653c055cf0b27fcdcd5258b5e83a00197b40953
Carlos Pavajeau <cantte098@gmail.com>
2022-05-06 12:09:57 -0500

    feat: add mongodb configuration for Price

--- src/main/kotlin/com/dew/common/domain/Price.kt
+++ src/main/kotlin/com/dew/common/domain/Price.kt
@@ -1,6 +1,20 @@
 package com.dew.common.domain
 
+import io.micronaut.core.annotation.Creator
 import io.micronaut.core.annotation.Introspected
+import org.bson.codecs.pojo.annotations.BsonCreator
+import org.bson.codecs.pojo.annotations.BsonProperty
+import javax.validation.constraints.NotBlank
 
 @Introspected
-data class Price(val amount: Float, val currency: String)
+data class Price @Creator @BsonCreator constructor(
+    @field:BsonProperty("amount")
+    @param:BsonProperty("amount")
+    @field:NotBlank
+    val amount: Float,
+
+    @field:BsonProperty("currency")
+    @param:BsonProperty("currency")
+    @field:NotBlank
+    val currency: String
+)
