9b521cdf201c4b177d7b785015236cadb249090a
Carlos Pavajeau <cantte098@gmail.com>
2022-07-06 09:52:45 -0500

    feat: add @ReflectiveAccess to price class

--- src/main/kotlin/com/dew/common/domain/Price.kt
+++ src/main/kotlin/com/dew/common/domain/Price.kt
@@ -2,11 +2,13 @@ package com.dew.common.domain
 
 import io.micronaut.core.annotation.Creator
 import io.micronaut.core.annotation.Introspected
+import io.micronaut.core.annotation.ReflectiveAccess
 import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonProperty
 import javax.validation.constraints.NotBlank
 
 @Introspected
+@ReflectiveAccess
 data class Price @Creator @BsonCreator constructor(
     @field:BsonProperty("amount")
     @param:BsonProperty("amount")
